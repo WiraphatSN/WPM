69a269782c0878a61fbb6148397ac672e2319c31
WiraphatSN <154593409+WiraphatSN@users.noreply.github.com>
2024-09-06 10:09:21 +0700

    Add files via upload

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,3 +1,15 @@
 $(window).on("load", function() {
     $(".loader-wrapper").fadeOut(1000);
 });
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const barmenu = document.getElementById('barmenu');
+    const offScreenMenu = document.querySelector('.off-screen-menu');
+
+    barmenu.addEventListener('click', (e) => {
+        e.preventDefault();
+        offScreenMenu.classList.toggle('active');
+    });
+});
